c127b37a53a2e8d6ed73443b5c322c705626ca06
arwST <105440994+zeroRW@users.noreply.github.com>
2023-02-16 20:15:22 -0600

    Inicio de menu
    Pequeño diseño de menu

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,12 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>MacuinDashboards</title>
 </head>
 <body>
-    
 
+    <div class="capa"></div>
+    <div class="container-menu">
+        <div class="cont-menu">
+            <nav>
+                <a href=""><  Registro Comics</a>
+                <a href=""> Registrar Articulos</a>
+                <a href=""> Consulta Comics</a>
+                <a href=""> Consulta Articulos</a>
+                <a href=""> Proveedores</a>
+                <a href=""> Consulta Proveedores</a>
+                <a href="">Pedidos</a>
+                <a href="">Ventas</a>
+                <a class="fw-bold" href="/">LOG OUT</a>
+            </nav>
+        </div>
+    </div>
+    
     @yield('codigo')
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
