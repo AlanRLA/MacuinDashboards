a9427bdf376956b1d2c1ca0b3c98392640682de9
arwST <105440994+zeroRW@users.noreply.github.com>
2023-02-16 19:50:02 -0600

    Vista cliente y ruta
    Se creo modelo de la vista y la ruta

--- app/Http/Controllers/controladorMacuin.php
+++ app/Http/Controllers/controladorMacuin.php
@@ -6,54 +6,38 @@ use Illuminate\Http\Request;
 
 class controladorMacuin extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+
+
+    //FUNCIONES LOGIN
+    public function loginInicio(){
         return view('login');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //FUNCIONES INDEX (CLIENTE, J-SOPORTE Y AUXILIAR)
+    public function indexCliente()
+    {
+        return view('macuinCliente');
+    }
+
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         //
